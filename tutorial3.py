c9b23670cf222cb971341bd07048fe1a002a1fae
yuki_fujisawa <g031y148yuki@gmail.com>
2021-10-30 15:12:01 +0900

    Update 学習

--- tutorial3.py
+++ tutorial3.py
@@ -3,13 +3,42 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(tf.__version__)
+class_names = ['Top', 'Trouser', 'Pullover', 'Dress',
+            'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Boot']
+
+def plot_image(i, predictions_array, true_label, img):
+  true_label, img = true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  true_label = true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-
-class_name = ['Top', 'Trouser', 'Pullover', 'Dress',
-            'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Boot']
 
 # print(type(train_images)) => <class 'numpy.ndarray'>
 # print(type(train_labels)) => <class 'numpy.ndarray'>
@@ -37,5 +66,23 @@ model.compile(optimizer='adam', #どうやって学習を最適化するかを�
               metrics=['accuracy']) #メトリックはとりあえずaccuracyを選んでおけば良い
 model.fit(train_images, train_labels, epochs=5)
 
-test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
-print(f"\nTest accuracy:{test_loss}")
+# test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+# print(f"\nTest accuracy:{test_loss}")
+
+predictions = model.predict(test_images)
+
+print(predictions[0])
+np.argmax(predictions[0])
+
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions[i], test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
+
